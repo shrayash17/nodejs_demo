4f09415e0feb290a52a3a872efbcaaca0ea5e696
shrayash-gawade <shreyashgawade2002@gmail.com>
2024-12-28 08:39:53 +0530

    next working

--- src/app.js
+++ src/app.js
@@ -2,11 +2,26 @@ const exp = require("express")
 
 const app = exp();
 
+app.use("/sant",[
+    (req,res,next)=>{
+        console.log("first")
+        next();
+    },
+    (req,resp,next)=>{
+        console.log("second")
+        next();
+
+    },
+    (req,res,next)=>{
+        console.log("third")
+        next()
+    },
+    (req,res)=>{
+        console.log("final")
+        res.send("last la alay ")
+    }
+])
+
 app.listen(7777,()=>{
-    console.log("listening to port 7777")
+    console.log("listen chalu ahe @ 7777")
 })
-
-app.use("/shet",(req,resp)=>{
-    resp.send("shet namaskar !")
-})
-
